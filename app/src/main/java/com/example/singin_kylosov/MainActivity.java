f1010468ca00d35fe95da194474eced7cfb8b2a1
MatveiKylosov <matveikyl@mail.ru>
2024-04-07 19:03:24 +0500

    fix

--- app/src/main/java/com/example/singin_kylosov/MainActivity.java
+++ app/src/main/java/com/example/singin_kylosov/MainActivity.java
@@ -3,11 +3,8 @@ package com.example.singin_kylosov;
 import android.os.Bundle;
 import android.view.MotionEvent;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
