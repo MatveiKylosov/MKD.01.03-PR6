4d3dc758a39d22f276adbf2a57bf1f087f58e06c
MatveiKylosov <matveikyl@mail.ru>
2024-04-07 19:27:26 +0500

    реализована страница reset, а также свайп вверх

--- app/src/main/java/com/example/singin_kylosov/MainActivity.java
+++ app/src/main/java/com/example/singin_kylosov/MainActivity.java
@@ -15,21 +15,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public int start_x=0;
+    public int start_y = 0;
 
     public boolean onTouchEvent(MotionEvent event)
     {
+
+
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                start_x=(int)event.getX();
+                start_x = (int)event.getX();
+                start_y = (int)event.getY();
                 break;
 
             case MotionEvent.ACTION_UP:
-                if(Math.abs((int)event.getX() - start_x) > 50){
-                    if(start_x < (int)event.getX()){
+                int end_x = (int)event.getX();
+                int end_y = (int)event.getY();
+
+                // свайп влево или вправо
+                if(Math.abs(end_x - start_x) > 50){
+                    if(start_x < end_x){
                         setContentView(R.layout.signin);
                     } else {
-                      setContentView(R.layout.regin);
+                        setContentView(R.layout.regin);
                     }
+                }
+
+                // свайп вверх
+                if(Math.abs(end_y - start_y) > 50 && start_y > end_y){
+                    setContentView(R.layout.reset);
                 }
                 break;
         }
