598ed601a401095d1a029543946c65739dcc68a8
MatveiKylosov <matveikyl@mail.ru>
2024-04-11 23:43:52 +0500

    Авторизация пользователя. Kylosov

--- app/src/main/java/com/example/singin_kylosov/MainActivity.java
+++ app/src/main/java/com/example/singin_kylosov/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.singin_kylosov;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -7,7 +8,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
 import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +29,49 @@ public class MainActivity extends AppCompatActivity {
         public void setPassword(String _password) { this.password = _password; }
         public String getPassword() { return this.password; }
     }
+    class GetDataUser extends AsyncTask<Void, Void, Void> {
+        String body;
+        @Override
+        protected Void doInBackground(Void... params) {
+            Document doc_b = null;
+            try {
+                doc_b = Jsoup.connect("https://0pp0site.000webhost.app.com/index.php?login=" + login + "&password=" + password).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (doc_b != null) {
+                body = doc_b.text();
+            } else body = "Ошибка!";
 
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            try {
+                if (body.length() != 0) {
+                    JSONArray jsonArray = new JSONArray(body);
+
+                    dataUser.clear();
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonRead = jsonArray.getJSONObject(i);
+
+                        DataUser duUser = new DataUser();
+                        duUser.setId(jsonRead.getString("id"));
+                        duUser.setLogin(jsonRead.getString("login"));
+                        duUser.setPassword(jsonRead.getString("password"));
+
+                        dataUser.add(duUser);
+                    }
+                    if (dataUser.size() != 0) {
+                        AlertDialog("Авторизация", "Пользователь авторизован.");
+                    }
+                } else AlertDialog("Авторизация", "Пользователя с таким логином и паролем не существует.");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
