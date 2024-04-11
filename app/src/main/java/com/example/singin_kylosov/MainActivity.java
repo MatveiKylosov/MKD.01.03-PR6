57b5851c41420ef7fbaab9787c4835317242d4ff
MatveiKylosov <matveikyl@mail.ru>
2024-04-11 23:35:55 +0500

    Создание класса DataUser. Kylosov

--- app/src/main/java/com/example/singin_kylosov/MainActivity.java
+++ app/src/main/java/com/example/singin_kylosov/MainActivity.java
@@ -2,11 +2,27 @@ package com.example.singin_kylosov;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
+    public class DataUser {
+        public String id;
+        public String login;
+        public String password;
+
+        public void setId(String _id) { this.id = _id; }
+        public String getId() { return this.id; }
+        public void setLogin(String _login) { this.login = _login; }
+        public String getLogin() { return this.login; }
+        public void setPassword(String _password) { this.password = _password; }
+        public String getPassword() { return this.password; }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +63,18 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return false;
+    }
+
+    public  String login, password;
+    public void onAutorization(View view)
+    {
+        TextView tv_login = findViewById(R.id.SigninLogin);
+        login = tv_login.getText().toString();
+
+        TextView tv_password = findViewById(R.id.SigninPassword);
+        password = tv_password.getText().toString();
+
+        GetDataUser gdu = new GetDataUser();
+        gdu.execute();
     }
 }
